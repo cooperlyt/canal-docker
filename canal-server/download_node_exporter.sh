5f31a3a322496df098a00f266d8410144981bef3
cooper <lyt3131@163.com>
2023-04-10 22:47:44 +0800

    A

--- canal-server/download_node_exporter.sh
+++ canal-server/download_node_exporter.sh
@@ -1,2 +1,8 @@
 cpu_arch=`arch`
+
+if [ "$cpu_arch" = "x86_64" ]
+then
+  cpu_arch = "amd64"
+fi
+
 wget "https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-$cpu_arch.tar.gz" -O /tmp/node_exporter.tar.gz
